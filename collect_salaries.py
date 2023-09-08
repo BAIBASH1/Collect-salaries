62daccd443f5cbc62911f0af08874311d3f4cf07
BAIBASH1 <gazinyr2014@mail.ru>
2023-09-08 17:10:59 +0300

    fix env variable
    add all enviroment variable to def main

--- collect_salaries.py
+++ collect_salaries.py
@@ -113,8 +113,7 @@ def collect_salaries_hh():
     return salaries_of_lang
 
 
-def collect_salaries_sj():
-    SECRET_KEY_SJ = os.environ['SECRET_KEY_SJ']
+def collect_salaries_sj(SECRET_KEY_SJ):
     headers = {
         'X-Api-App-Id': SECRET_KEY_SJ
     }
@@ -168,7 +167,8 @@ def get_tables(salaries_of_lang, site_name):
 
 def main():
     load_dotenv()
-    salaries_sj = collect_salaries_sj()
+    SECRET_KEY_SJ = os.environ['SECRET_KEY_SJ']
+    salaries_sj = collect_salaries_sj(SECRET_KEY_SJ)
     print(get_tables(salaries_sj, 'Superjob Moscow'))
     salaries_hh = collect_salaries_hh()
     print(get_tables(salaries_hh, 'HeadHunter Moscow'))
